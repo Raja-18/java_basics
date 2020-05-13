66ddb5754a8c63fbd6375edacbe65c5db637b24d
Raja-18 <60438105+Raja-18@users.noreply.github.com>
2020-05-13 21:23:16 +0530

    updated with getting user input
    added Scanner function to the program that gets input from users

--- fibonacci.java
+++ fibonacci.java
@@ -1,11 +1,16 @@
-public class fibonacci {             // class name should be the file name
+import java.util.Scanner;                            //for Scanner
+public class fibonacci {                       // class name should be the file name
     public static void main(String[] args) {   
-        int a=1,b=0,c=0,n=10;              // initialising the variables 
-        for(int i=0;i<n;i++){              // "for loop" for iteration 
+        int a=1,b=0,c=0;                       // initialising the variables
+        Scanner scan=new Scanner(System.in);   //initialising scan as Scanner object
+        System.out.print("enter the number: ");
+        int n=scan.nextInt();                  //getting integer as input from user
+        for(int i=0;i<n;i++){                  // "for loop" for iteration 
             System.out.print(c+" ");       
             c=a+b;
             a=b;
             b=c;
+        scan.close()
         }
     }
 }
