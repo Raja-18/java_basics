368f6a5212fc9254868ce6c4e2579d3ebdb773db
Raja-18 <60438105+Raja-18@users.noreply.github.com>
2020-05-14 18:47:43 +0530

    added my github link

--- fibonacci.java
+++ fibonacci.java
@@ -1,15 +1,22 @@
+/*by Rajasekaran (https://github.com/Raja-18)
+*/
+
 import java.util.Scanner;                            //for Scanner
+
 public class fibonacci {                       // class name should be the file name
     public static void main(String[] args) {   
         int a=1,b=0,c=0;                       // initialising the variables
+        
         Scanner scan=new Scanner(System.in);   //initialising scan as Scanner object
         System.out.print("enter the number: ");
         int n=scan.nextInt();                  //getting integer as input from user
+        
         for(int i=0;i<n;i++){                  // "for loop" for iteration 
             System.out.print(c+" ");       
             c=a+b;
             a=b;
-            b=c;
+            b=c;    
+            
         scan.close()
         }
     }
