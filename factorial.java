7f468f33e8e35d7ab1b51bfe05e565ddb6f7bfde
Raja-18 <60438105+Raja-18@users.noreply.github.com>
2020-05-14 18:50:34 +0530

    added my github link

--- factorial.java
+++ factorial.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+/*by Rajasekaran (https://github.com/Raja-18)
+*/
+
+import java.util.Scanner;                                          //Scanner for getting user input
+
 public class factorial {
     public static void main(String[] args) {
         int n;
+        
         Scanner scan=new Scanner(System.in);
         System.out.print("enter the number: ");
         n=scan.nextInt();
+        
         int fact=1,i=0;
         while(i<n){
             fact=fact*(n-i);
             i+=1;
         }
         System.out.print(fact+" ");
+        
         scan.close();
     }
     
